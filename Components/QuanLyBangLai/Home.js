a9a78ad19f7ac9114183c12d2c157cd2edf44881
Kha Pham <khapham.7165@gmail.com>
2021-01-15 11:50:02 +0700

    add UI - Canh bao ket xe - Quan ly bang lai

--- Components/QuanLyBangLai/Home.js
+++ Components/QuanLyBangLai/Home.js
@@ -2,32 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, SafeAreaView, TouchableHighlight, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import {axios} from 'axios';
 
 export const QuanLyBangLai = () => {
     const [inputText, onChangeText] = useState('');
-    const [data, setData] = useState([
-        {
-            name: 'A1',
-            description: 'Day la bang lai A1',
-        },
-        {
-            name: 'A2',
-            description: 'Day la bang lai A2',
-        }
-    ]);
+    const [data, setData] = useState([{"$id":"1","BanglaiId":2,"LoaiBang":"A2","ThongTin":"Cho xe may","ChuxevaBanglais":null}]);
+    const [bangLai, setBangLai] = useState(null);
 
-    const BangLai = data.map((item, i) => {
+    const navigation = useNavigation();
+
+    const BangLai = bangLai && bangLai.map((item, i) => { //render Bang lai
         return (
             <View>
                 <View style = {{height: 15}}/>
                 <View style = {{backgroundColor: 'white', padding: 15, borderRadius: 10}}>
-                    <Text>Loại bằng lái: {data[i].name}</Text>
-                    <Text>Chi tiết: {data[i].description}</Text>
+                    <Text>Loại bằng lái: {item.LoaiBang}</Text>
+                    <Text>Thông tin: {item.ThongTin}</Text>
                 </View>
             </View>
         )
     })
-    const navigation = useNavigation();
     return (
         <SafeAreaView style = {{padding: 15, alignSelf:'stretch'}}>
             <View style = {{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
@@ -37,14 +31,17 @@ export const QuanLyBangLai = () => {
                     value={inputText}
                     placeholder = {'Bằng lái cần tìm'}
                 />
-                <TouchableHighlight onPress={() => {}} underlayColor = 'white'>
+                <TouchableHighlight onPress={async() => {
+                    const result = await axios.get(`https://localhost:44399/api/banglai/${inputText}`);
+                    setBangLai(result.data);
+                }} underlayColor = 'white'>
                     <Image
                         source = {{uri: 'https://static.thenounproject.com/png/3134345-200.png'}}
                         style = {{width: 40, height: 40, resizeMode: 'contain', tintColor: 'black'}}
                     />
                 </TouchableHighlight>
             </View>
-            {BangLai}
+            {bangLai && BangLai}
         </SafeAreaView>
     )
 }
